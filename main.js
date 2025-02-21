2270a6b899f41a4ee86f6405e66c7b282091387f
lijiamao <951572198@qq.com>
2025-02-21 21:56:30 +0800

    custom

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // 导入模块
-const { app, BrowserWindow, ipcMain, Menu, dialog } = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
 
 const createWindow = () => {
@@ -7,54 +7,20 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    backgroundColor: '#fdfdfd',
-    // autoHideMenuBar: true, // 隐藏菜单栏，用户仍然可以通过按下 Alt 键临时显示菜单栏
-    // frame: false, // 移除标题栏和菜单栏
     webPreferences: {
-      nodeIntegration: true, // 控制渲染进程是否可以访问 Node.js 的 API
-      contextIsolation: true, // 控制渲染进程的 JavaScript 上下文是否与 Electron 的内部上下文隔离
       preload: path.join(__dirname, 'preload.js')
     },
   })
 
-  const menu = Menu.buildFromTemplate([
-    {
-      label: '操作数据',
-      submenu: [
-        {
-          click: () => win.webContents.send('update-counter', 1),
-          label: 'Increment'
-        },
-        {
-          click: () => win.webContents.send('update-counter', -1),
-          label: 'Decrement'
-        }
-      ]
-    }
 
-  ])
-  Menu.setApplicationMenu(menu)
-
-  ipcMain.on('set-title', handleSetTitle)
-  ipcMain.handle('dialog:openFile', handleFileOpen)
-
-  win.loadFile('index.html')
-  // win.loadURL('https://chat18.aichatos98.com')
-
-  // 移除默认菜单
-  // Menu.setApplicationMenu(null);
-  // 动态隐藏菜单栏
-  // win.setMenuBarVisibility(false);
+  // win.loadFile('index.html')
+  win.loadURL('https://example.com')
 
   win.webContents.openDevTools();
 }
 
 // 控制应用程序的事件生命周期
 app.on('ready', () => {
-  ipcMain.handle('ping', () => 'pong')
-  ipcMain.on('counter-value', (_event, value) => {
-    console.log(value) // will print value to Node console
-  })
   createWindow()
 })
 
@@ -65,26 +31,3 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
-
-function handleSetTitle(event, title) {
-  const webContents = event.sender
-  const win = BrowserWindow.fromWebContents(webContents)
-  win.setTitle(title)
-}
-
-async function handleFileOpen () {
-  const { canceled, filePaths } = await dialog.showOpenDialog({
-    properties: ['openFile'],
-    filters: [
-      { name: 'Images', extensions: ['jpg', 'png', 'gif'] },
-      { name: 'Movies', extensions: ['mkv', 'avi', 'mp4'] },
-      { name: 'Custom File Type', extensions: ['as'] },
-      { name: 'All Files', extensions: ['*'] }
-    ]
-  })
-  if (!canceled) {
-    return filePaths[0]
-  } else {
-    return ''
-  }
-}
--- preload.js
+++ preload.js
@@ -4,19 +4,4 @@ contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
   electron: () => process.versions.electron,
-  ping: () => ipcRenderer.invoke('ping'),
-  // 除函数之外，我们也可以暴露变量
-  a: 1,
-  b: 2,
-})
-
-contextBridge.exposeInMainWorld('myApi', {
-  desktop: true,
-})
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  setTitle: (title) => ipcRenderer.send('set-title', title),
-  openFile: () => ipcRenderer.invoke('dialog:openFile'),
-  onUpdateCounter: (callback) => ipcRenderer.on('update-counter', (_event, value) => callback(value)),
-  counterValue: (value) => ipcRenderer.send('counter-value', value)
 })
